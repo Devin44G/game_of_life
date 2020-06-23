172546bb336d1cac9c2da092b7dc5a13328a5e91
Devin44G <35251347+Devin44G@users.noreply.github.com>
2020-06-23 13:05:14 -0500

    Animation and rules of game functioning

--- README.md
+++ README.md
@@ -7,11 +7,12 @@
   - [x] Decide upon a Canvas-based Grid **or** an Array-based Grid?
   - [x] Define grid size
   - [x] Get grid rendering
-  - [ ] Produce <b>*Still Lifes*</b>
+  - [ ] Produce pre-set <b>*Still Lifes*</b>
   - [x] Add click event to fill in a grid block with color to signify "dead" or "alive" blocks (for testing purposes)
   - [ ] Implement basic "click-through" animation (doesn't yet follow any set of rules)
-  - [ ] Produce <b>*Oscillators*</b>
+  - [ ] Produce pre-set <b>*Oscillators*</b>
 - [ ] Games Rules Complete
-  - [ ] Tile can "look" for neighboring tiles relative to its position
-  - [ ] Introduce game rules
+  - [x] Tile can "look" for neighboring tiles relative to its position
+  - [x] Introduce game rules to logic
+  - [ ] Introduce a readable *rulebook*
 - [ ] Game complete
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,12 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback, useRef } from 'react';
 import produce from 'immer';
 
 
 const Grid = () => {
   const colNum = 25;
   const rowNum = 25;
+  const operations = [
+    [0, 1],
+    [0, -1],
+    [1, -1],
+    [-1, 1],
+    [1, 1],
+    [-1, -1],
+    [1, 0],
+    [-1, 0]
+  ];
 
-  const [alive, setAlive] = useState(false);
+  const [started, setStarted] = useState(false);
+  const startedRef = useRef(started);
+        startedRef.current = started;
+
+  const runAnim = useCallback(() => {
+    if(!startedRef.current) {
+      return;
+    }
+
+    setGrid((grid) => {
+      return produce(grid, gridCopy => {
+        for(let i = 0; i < rowNum; i++) {
+          for(let j = 0; j < colNum; j++) {
+            let neighbors = 0;
+            operations.forEach(([x, y]) => {
+              const newI = i + x;
+              const newJ = j + y;
+              if(newI >= 0 && newI < rowNum && newJ >= 0 && newJ < colNum) {
+                neighbors += grid[newI][newJ];
+              }
+            });
+
+            if(neighbors < 2 || neighbors > 3) {
+              gridCopy[i][j] = 0;
+            } else if(grid[i][j] === 0 && neighbors === 3) {
+              gridCopy[i][j] = 1;
+            }
+          }
+        }
+      });
+    });
+
+    setTimeout(runAnim, 1000);
+  }, [operations]);
+
   const [grid, setGrid] = useState(() => {
     const rows = [];
     for(let i = 0; i < colNum; i++) {
@@ -16,32 +60,43 @@ const Grid = () => {
   });
 
   return(
-    <div
-      style={{
-        marginTop: '2rem',
-        marginBottom: '2rem',
-        display: 'grid',
+    <>
+      <button
+        onClick={() => {
+          setStarted(!started);
+          if(!started) {
+            startedRef.current = true;
+            runAnim();
+          }
+        }}
+      >{started ? 'Stop' : 'Start'}</button>
+      <div
+        style={{
+          marginTop: '2rem',
+          marginBottom: '2rem',
+          display: 'grid',
         gridTemplateColumns: `repeat(${colNum}, 1.5rem)`,
-        justifyContent: 'center'
-      }}>
-      { grid.map((rows, i) =>
-        rows.map((cols, j) =>
-          <div
-            onClick={() => {
-              setGrid(produce(grid, gridCopy => {
-                gridCopy[i][j] = grid[i][j] ? 0 : 1;
-              }))
-            }}
-            key={`${i}-${j}`}
-            style={{
-              width:'1.5rem',
-              height:'1.5rem',
-              border:'solid 1px black',
-              backgroundColor: (grid[i][j] == 1) ? 'darkslateblue' : '#f1f1f1'
-            }}>
-          </div>)
-      )}
-    </div>
+          justifyContent: 'center'
+        }}>
+        { grid.map((rows, i) =>
+          rows.map((cols, j) =>
+            <div
+              onClick={() => {
+                setGrid(produce(grid, gridCopy => {
+                  gridCopy[i][j] = grid[i][j] ? 0 : 1;
+                }))
+              }}
+              key={`${i}-${j}`}
+              style={{
+                width:'1.5rem',
+                height:'1.5rem',
+                border:'solid 1px black',
+                backgroundColor: (grid[i][j] === 1) ? 'darkslateblue' : '#f1f1f1'
+              }}>
+            </div>)
+        )}
+      </div>
+    </>
   );
 };
 
