1c0ae482e737f792a78aaa74c6cb2f4e82f5cac3
Devin44G <35251347+Devin44G@users.noreply.github.com>
2020-06-23 11:58:34 -0500

    Added click event to change grid square color to signify whether a particular cell is dead or alive

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
   - [x] Define grid size
   - [x] Get grid rendering
   - [ ] Produce <b>*Still Lifes*</b>
-  - [ ] Add click event to fill in a grid block with color to signify "dead" or "alive" blocks (for testing purposes)
+  - [x] Add click event to fill in a grid block with color to signify "dead" or "alive" blocks (for testing purposes)
   - [ ] Implement basic "click-through" animation (doesn't yet follow any set of rules)
   - [ ] Produce <b>*Oscillators*</b>
 - [ ] Games Rules Complete
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import produce from 'immer';
 
 
 const Grid = () => {
   const colNum = 25;
   const rowNum = 25;
 
+  const [alive, setAlive] = useState(false);
   const [grid, setGrid] = useState(() => {
     const rows = [];
     for(let i = 0; i < colNum; i++) {
@@ -14,20 +16,28 @@ const Grid = () => {
   });
 
   return(
-    <div style={{
-      marginTop: '2rem',
-      display: 'grid',
-      gridTemplateColumns: `repeat(${colNum}, 2rem)`,
-      justifyContent: 'center'
-    }}>
+    <div
+      style={{
+        marginTop: '2rem',
+        marginBottom: '2rem',
+        display: 'grid',
+        gridTemplateColumns: `repeat(${colNum}, 1.5rem)`,
+        justifyContent: 'center'
+      }}>
       { grid.map((rows, i) =>
         rows.map((cols, j) =>
           <div
+            onClick={() => {
+              setGrid(produce(grid, gridCopy => {
+                gridCopy[i][j] = grid[i][j] ? 0 : 1;
+              }))
+            }}
             key={`${i}-${j}`}
             style={{
-              width:'2rem',
-              height:'2rem',
-              border:'solid 1px black'
+              width:'1.5rem',
+              height:'1.5rem',
+              border:'solid 1px black',
+              backgroundColor: (grid[i][j] == 1) ? 'darkslateblue' : '#f1f1f1'
             }}>
           </div>)
       )}
